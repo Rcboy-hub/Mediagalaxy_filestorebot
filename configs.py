4cd097941c9d2c27851cc846aff0f97de452678e
Rcboy-hub <harishentreprenur@gmail.com>
2025-01-13 02:46:59 -0800

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,17 +1,17 @@
 import os
 
 class Config(object):
-  API_ID = int(os.environ.get("API_ID", ""))
-  API_HASH = os.environ.get("API_HASH", "")
-  BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-  BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
-  DB_CHANNEL = int(os.environ.get("DB_CHANNEL", ""))
-  SHORTLINK_URL = os.environ.get('SHORTLINK_URL', "MoneyKamalo.com")
-  SHORTLINK_API = os.environ.get('SHORTLINK_API', "0eefb93e1e3ce9470a7033115ceb1bad13a9d674")
-  BOT_OWNER = int(os.environ.get("BOT_OWNER", ""))
-  DATABASE_URL = os.environ.get("DATABASE_URL", "")
-  UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "")
-  LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+  API_ID = int(os.environ.get("API_ID", "21772072"))
+  API_HASH = os.environ.get("API_HASH", " df3c032880b3f1e1bbf712109283f967")
+  BOT_TOKEN = os.environ.get("BOT_TOKEN", "8045370089:AAF3tstDzz8qQpaQynRmC6O58uUM6JSbwuI")
+  BOT_USERNAME = os.environ.get("BOT_USERNAME", "Anime_Archive_bot")
+  DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "1002410550265"))
+  SHORTLINK_URL = os.environ.get('SHORTLINK_URL', "")
+  SHORTLINK_API = os.environ.get('SHORTLINK_API', "")
+  BOT_OWNER = int(os.environ.get("BOT_OWNER", "1002251851952"))
+  DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://harish:harish%402606@cluster0.wfs2u.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+  UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "1002251851952")
+  LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "1002469428913"))
   BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
   FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
   BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", True))
@@ -23,7 +23,7 @@ Send Me any Media or File. I can Work In Channel too. Add Me to Channel with Edi
 
 ╭────[ 🔅FɪʟᴇSᴛᴏʀᴇBᴏᴛ🔅]────⍟
 │
-├🔸 My Name: [FileStore Bot](https://t.me/{BOT_USERNAME})
+├🔸 My Name: [Media galaxy FileStore Bot](https://t.me/{Anime_Archive_bot})
 │
 ├🔸 Language: [Python 3](https://www.python.org)
 │
